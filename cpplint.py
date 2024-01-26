5cc5616186f0dccd71598cba7e37beb822d21a45
Aaron Liu <aaronliu0130@gmail.com>
2024-01-26 17:42:42 -0500

    add new headers from C++23 and C23

--- cpplint.py
+++ cpplint.py
@@ -409,7 +409,7 @@ _CPP_HEADERS = frozenset([
     'alloc.h',
     'builtinbuf.h',
     'bvector.h',
-    # 'complex.h', collides with System C header "complex.h"
+    # 'complex.h', collides with System C header "complex.h" since C11
     'defalloc.h',
     'deque.h',
     'editbuf.h',
@@ -455,7 +455,7 @@ _CPP_HEADERS = frozenset([
     'tree.h',
     'type_traits.h',
     'vector.h',
-    # 17.6.1.2 C++ library headers
+    # C++ library headers
     'algorithm',
     'array',
     'atomic',
@@ -509,9 +509,9 @@ _CPP_HEADERS = frozenset([
     'utility',
     'valarray',
     'vector',
-    # 17.6.1.2 C++14 headers
+    # C++14 headers
     'shared_mutex',
-    # 17.6.1.2 C++17 headers
+    # C++17 headers
     'any',
     'charconv',
     'codecvt',
@@ -521,7 +521,7 @@ _CPP_HEADERS = frozenset([
     'optional',
     'string_view',
     'variant',
-    # 17.6.1.2 C++20 headers
+    # C++20 headers
     'barrier',
     'bit',
     'compare',
@@ -537,7 +537,17 @@ _CPP_HEADERS = frozenset([
     'stop_token',
     'syncstream',
     'version',
-    # 17.6.1.2 C++ headers for C library facilities
+    # C++23 headers
+    'expected',
+    'flat_map',
+    'flat_set',
+    'generator',
+    'mdspan',
+    'print',
+    'spanstream',
+    'stacktrace',
+    'stdfloat',
+    # C++ headers for C library facilities
     'cassert',
     'ccomplex',
     'cctype',
@@ -598,6 +608,9 @@ _C_HEADERS = frozenset([
     'uchar.h',
     'wchar.h',
     'wctype.h',
+    # C23 headers
+    'stdbit.h',
+    'stdckdint.h',
     # additional POSIX C headers
     'aio.h',
     'arpa/inet.h',
